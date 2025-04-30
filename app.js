d77e5d1d1ff024e86ce99af0cdd246d718131c9b
Muhammad Ahmed <ahmedsilat95@gmail.com>
2025-04-30 23:25:21 +0500

    quiz questions has been putted in a separate file

--- app.js
+++ app.js
@@ -1,38 +1,4 @@
-﻿const quizData = [
-  {
-    question: "What does HTML stand for?",
-    options: [
-      "HyperText Markup Language.",
-      "Hyper Transfer Markup Language.",
-      "HighText Markup Language.",
-      "HyperText Machine Language.",
-    ],
-    correct: 0,
-  },
-
-  {
-    question: "Which tag is used to define an unordered list in HTML?",
-    options: ["<ol>", "<ul>", "<li>", "<list>"],
-    correct: 1,
-  },
-
-  {
-    question: "How do you add a comment in HTML?",
-    options: [
-      "<!--- This is a comment --->",
-      "// This is a comment",
-      "/* This is a comment */",
-      "<!-- This is a comment -->",
-    ],
-    correct: 3,
-  },
-
-  {
-    question: "Which tag is used to define an ordered list in HTML?",
-    options: ["<li>", "<ul>", "<ol>", "<list>"],
-    correct: 2,
-  },
-];
+﻿import { quizData } from "./questions.js";
 
 const quiz = document.querySelector(".quiz-card");
 const timer = document.querySelector("#clock");
---
+++ questions.js
@@ -0,0 +1,35 @@
+export const quizData = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "HyperText Markup Language.",
+      "Hyper Transfer Markup Language.",
+      "HighText Markup Language.",
+      "HyperText Machine Language.",
+    ],
+    correct: 0,
+  },
+
+  {
+    question: "Which tag is used to define an unordered list in HTML?",
+    options: ["<ol>", "<ul>", "<li>", "<list>"],
+    correct: 1,
+  },
+
+  {
+    question: "How do you add a comment in HTML?",
+    options: [
+      "<!--- This is a comment --->",
+      "// This is a comment",
+      "/* This is a comment */",
+      "<!-- This is a comment -->",
+    ],
+    correct: 3,
+  },
+
+  {
+    question: "Which tag is used to define an ordered list in HTML?",
+    options: ["<li>", "<ul>", "<ol>", "<list>"],
+    correct: 2,
+  },
+];
